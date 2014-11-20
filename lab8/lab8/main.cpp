381b1282034411153552094b0e85cabc1a05050c
Anahit <indianvip60@gmail.com>
2014-11-19 20:44:59 -0800

    v1

---
+++ lab8/lab8/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  lab8
+//
+//  Created by senu on 11/19/14.
+//  Copyright (c) 2014 senu. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   
+    
+    
+    
+    
+    
+    return 0;
+}
